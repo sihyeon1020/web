4a8b27113905ae50ec97ce3c941360b7268c6a58
sihyeon1020 <124111013+sihyeon1020@users.noreply.github.com>
2023-02-02 17:34:24 +0900

    day04 프로젝트

---
+++ day04/out.js
@@ -0,0 +1,21 @@
+ //   function 빨래하다(옷들, 물, 세제, 시간(입력값)) {      }
+    // 함수는 여러 절차가 있는 처리를 이름을 부여하여 기능을 정의한다. 
+    function 빨래하다() {
+        console.log('1. 먼지를 털자.')
+        console.log('2. 물에 집어 넣는다.')
+        console.log('3. 비누를 바른다.')
+        console.log('4. 오물조물 비빈다.')
+        console.log('5. 물에 여러번 헹군다.')
+        console.log('6. 짠다.')
+        console.log('7. 넌다')
+    }
+    빨래하다() // 함수를 사용, 기능을 사용하는 것.
+
+    function 청소하다(){
+        console.log('1. 쓰레기를 버린다.')
+        console.log('2. 정리를 한다.')
+        console.log('3. 먼지를 털어낸다.')
+        console.log('1. 물걸레로 닦는다.')
+    }
+    
+    청소하다()
---
+++ day04/out2.js
@@ -0,0 +1,9 @@
+function 여행하다() {
+    console.log('1.여행하다.')
+    console.log('2.사진을 찍다.')
+}
+
+function 쉬다() {
+    console.log('1.쉬다.')
+    console.log('2.잠을 자다.')
+}
